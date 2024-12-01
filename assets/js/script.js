f8e168c1808e10e66600e92ce8af845355e5871d
Taha <mohammdtahaabdinasab@gmail.com>
2024-12-02 00:21:58 +0330

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 function CheckingDarkmode(){
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.style.background = "none";
     document.body.style.backgroundColor = "#3a3a3a";
   }
 }
